57ff4119e8cbb6c4db52460729982da31d2fee18
PierreLouis98 <pierre-louis.de-soultrait@edu.ece.fr>
2018-04-10 16:43:26 +0200

    class traffic light

--- traffic_light.cpp
+++ traffic_light.cpp
@@ -1,3 +1,11 @@
 #include "traffic_light.h"
 
 using namespace std;
+
+Traffic_light::Traffic_light(int etat, int posx, int posy)
+{
+    m_etat = etat;
+    m_posx = posx;
+    m_posy = posy;
+    m_idx = 0;
+}
